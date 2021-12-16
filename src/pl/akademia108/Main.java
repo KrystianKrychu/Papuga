c66e05b7a08f90248c9ad800b05bf6e5a9f9bf17
Krystian Wieczorek <krystianwieczorek70@gmail.com>
2021-12-16 13:58:37 +0100

    Dodano Kra, kra, papuga, papuga

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -1,8 +1,20 @@
 package pl.akademia108;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Kra, kra, papuga, papuga");
+        System.out.println("Wprowadź wyraz:");
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+        System.out.println("Kra, " + word);
+
+        System.out.println("Wprowadź wyraz:");
+        String word2 = scan.next();
+        System.out.println("Papuga, " + word2);
+
+        System.out.println("Autorem programu jest Krystian");
+        scan.close();
+
+
     }
 }
